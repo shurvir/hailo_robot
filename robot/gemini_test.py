2be929afec5cfeb7a323758f479d45e87365e98c
shurvir <shurvir.harrilal@gmail.com>
2024-10-21 11:28:51 +0200

    removed test file

--- robot/gemini_test.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-import os
-
-API_KEY = os.environ["GEMINI_API_KEY"]
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
